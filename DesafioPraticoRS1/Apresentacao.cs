29a5f7f639b729cb1b9c0f73cd0d284f7cd8e5d5
jampus90 <alexandre.yama2@gmail.com>
2026-03-31 00:33:44 -0300

    Primeiro commit exercícios Rocketseat 1

---
+++ DesafioPraticoRS1/Apresentacao.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioPraticoRS1;
+public class Apresentacao
+{
+    public string PerguntaNome()
+    {
+        Console.WriteLine("Qual seu nome?");
+        string nome = Console.ReadLine();
+        return nome;
+    }
+    public string PerguntaSobrenome()
+    {
+        Console.WriteLine("Qual seu sobrenome?");
+        string sobrenome = Console.ReadLine();
+        return sobrenome;
+    }
+    public void BemVindo(string nome)
+    {
+        Console.WriteLine($"Olá {nome}! Seja muito bem vindo!");
+    }
+    public void ConcatenaNomeSobrenome(string nome, string sobrenome)
+    {
+        Console.WriteLine($"{nome} {sobrenome}");
+    }
+}
---
+++ DesafioPraticoRS1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace DesafioPraticoRS1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var apresentacaoService = new Apresentacao();
+
+            Console.WriteLine("Escolha o exercício que deseja rodar:");
+            Console.WriteLine("1. Mensagem de boas vindas ao digitar o nome");
+            Console.WriteLine("2. Concatenar nome e sobrenome inserido pelo usuario");
+            Console.WriteLine("3. Programa que faz operações com 2 valores do tipo double");
+            Console.WriteLine("4. Programa que conta quantidade de caracteres inseridos");
+            Console.WriteLine("5. Programa para verificar placa de carro, seguindo padrão de 2018");
+            Console.WriteLine("6. Programa de exibição de data");
+
+            int exercicio = int.Parse(Console.ReadLine());
+            switch (exercicio) {
+                case 1:
+                    string nome  = apresentacaoService.PerguntaNome();
+                    apresentacaoService.BemVindo(nome);
+                    break;
+                case 2:
+                    string nome2 = apresentacaoService.PerguntaNome();
+                    string sobrenome = apresentacaoService.PerguntaSobrenome();
+                    apresentacaoService.ConcatenaNomeSobrenome(nome2, sobrenome);
+                    break;
+            }
+        }
+    }
+}
